478c1fce39b3595dd6d088230e3da3123663afc6
noaFeldberg <noa7feld@gmail.com>
2022-12-26 08:14:29 +0200

    cleanup

--- src/app/store/reducers/class.reducer.ts
+++ src/app/store/reducers/class.reducer.ts
@@ -7,11 +7,13 @@ export function classReducer(state: ClassData[] = [], action: ClassActions.Actio
   switch (action.type) {
     case ClassActions.ADD_CLASS:
       return [...state, action.payload]
+
     case ClassActions.REMOVE_CLASS:
       let newState = [...state];
       const index = newState.findIndex((c) => c.id === action.payload);
       newState.splice(index, 1);
       return newState;
+      
     default:
       return state;
   }
--- src/app/store/reducers/employee.reducer.ts
+++ src/app/store/reducers/employee.reducer.ts
@@ -4,20 +4,24 @@ import { Employee } from '../models/employee.model';
 
 
 export function employeeReducer(state: Employee[] = [], action: EmployeeActions.Actions) {
+  let index;
+  let newState;
+
   switch (action.type) {
     case EmployeeActions.ADD_EMPLOYEE:
       return [...state, action.payload]
+
     case EmployeeActions.REMOVE_EMPLOYEE:
-      let newState = [...state];
-      const index = newState.findIndex((c) => c.id === action.payload);
+      newState = [...state];
+      index = newState.findIndex((c) => c.id === action.payload);
       newState.splice(index, 1);
       return newState;
+
     case EmployeeActions.CHANGE_EMPLOYEE_CLASS:
-      let newState1 = [...state];
-      const index1 = newState1.findIndex((c) => c.id === action.payload.id);
-      newState1.splice(index1, 1);
-      const res = [...newState1, action.payload];
-      return [...newState1, action.payload]
+      newState = [...state];
+      index = newState.findIndex((c) => c.id === action.payload.id);
+      newState.splice(index, 1);
+      return [...newState, action.payload]
     default:
       return state;
   }
